64bdc08fc860ac6faf22069aea381d7c37f41e8f
richmondu <richmond.umagat@yahoo.com>
2020-09-30 04:02:37 +0800

    Revert

--- restapi/src/rest_api_config.py
+++ restapi/src/rest_api_config.py
@@ -6,5 +6,5 @@ class config:
     debugging = int(os.environ['CONFIG_USE_DEBUG_MODE'])
     CONFIG_HTTP_USE_TLS         = False
     CONFIG_HTTP_HOST            = "localhost"
-    CONFIG_HTTP_PORT            = 8001
+    CONFIG_HTTP_PORT            = 8000
     CONFIG_HTTP_TLS_PORT        = 443
