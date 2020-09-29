1710bd34b61bca5540a59a8676d87879152ee3c3
richmondu <richmond.umagat@yahoo.com>
2020-09-30 04:08:12 +0800

    Add live test

---
+++ _curltest/test_curl_live_GFI.bat
@@ -0,0 +1,11 @@
+set GET_API=https://richmondu.com/api/v1/shipping/fee
+set VOUCHER_CODE=GFI
+
+curl -X GET "%GET_API%?code=%VOUCHER_CODE%&weight=51&height=1&width=1&length=1" -H "accept: application/json"
+curl -X GET "%GET_API%?code=%VOUCHER_CODE%&weight=11&height=1&width=1&length=1" -H "accept: application/json"
+curl -X GET "%GET_API%?code=%VOUCHER_CODE%&weight=1&height=10&width=10&length=10" -H "accept: application/json"
+curl -X GET "%GET_API%?code=%VOUCHER_CODE%&weight=1&height=20&width=10&length=10" -H "accept: application/json"
+curl -X GET "%GET_API%?code=%VOUCHER_CODE%&weight=1&height=30&width=10&length=10" -H "accept: application/json"
+
+pause
+
