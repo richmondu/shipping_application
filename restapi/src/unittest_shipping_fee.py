3e9a5a9c3dce6257f2347712c3ca71e90ecb8617
richmondu <richmond.umagat@yahoo.com>
2020-09-30 04:24:11 +0800

    Update unittest_shipping_fee.py

--- restapi/src/unittest_shipping_fee.py
+++ restapi/src/unittest_shipping_fee.py
@@ -6,67 +6,67 @@ class ShippingFeeUnitTest(unittest.TestCase):
 
 	def test_RejectParcel_MYNT(self):
 		shipping = ShippingFee("MYNT", 51, 1, 1, 1)
-		self.assertEqual(shipping.get_fee(), "N/A")
+		self.assertEqual(shipping.get_fee(), ("N/A", "N/A", "N/A"))
 
 	def test_RejectParcel_GFI(self):
 		shipping = ShippingFee("GFI", 51, 1, 1, 1)
-		self.assertEqual(shipping.get_fee(), "N/A")
+		self.assertEqual(shipping.get_fee(), ("N/A", "N/A", "N/A"))
 
 	def test_RejectParcel_skdlks(self):
 		shipping = ShippingFee("skdlks", 51, 1, 1, 1)
-		self.assertEqual(shipping.get_fee(), "N/A")
+		self.assertEqual(shipping.get_fee(), ("N/A", "N/A", "N/A"))
 
 
 	def test_HeavyParcel_MYNT(self):
 		shipping = ShippingFee("MYNT", 11, 1, 1, 1)
-		self.assertEqual(shipping.get_fee(), "193.05")
+		self.assertEqual(shipping.get_fee(), ("193.05", "12.25%", "220"))
 
 	def test_HeavyParcel_GFI(self):
 		shipping = ShippingFee("GFI", 11, 1, 1, 1)
-		self.assertEqual(shipping.get_fee(), "203.5")
+		self.assertEqual(shipping.get_fee(), ("203.5", "7.5%", "220"))
 
 	def test_HeavyParcel_skdlks(self):
 		shipping = ShippingFee("skdlks", 11, 1, 1, 1)
-		self.assertEqual(shipping.get_fee(), "220")
+		self.assertEqual(shipping.get_fee(), ("220", "0%", "220"))
 
 
 	def test_SmallParcel_MYNT(self):
 		shipping = ShippingFee("MYNT", 1, 10, 10, 10)
-		self.assertEqual(shipping.get_fee(), "26.325")
+		self.assertEqual(shipping.get_fee(), ("26.325", "12.25%", "30.0"))
 
 	def test_SmallParcel_GFI(self):
 		shipping = ShippingFee("GFI", 1, 10, 10, 10)
-		self.assertEqual(shipping.get_fee(), "27.75")
+		self.assertEqual(shipping.get_fee(), ("27.75", "7.5%", "30.0"))
 
 	def test_SmallParcel_skdlks(self):
 		shipping = ShippingFee("skdlks", 1, 10, 10, 10)
-		self.assertEqual(shipping.get_fee(), "30.0")
+		self.assertEqual(shipping.get_fee(), ("30.0", "0%", "30.0"))
 
 
 	def test_MediumParcel_MYNT(self):
 		shipping = ShippingFee("MYNT", 1, 20, 10, 10)
-		self.assertEqual(shipping.get_fee(), "70.2")
+		self.assertEqual(shipping.get_fee(), ("70.2", "12.25%", "80.0"))
 
 	def test_MediumParcel_GFI(self):
 		shipping = ShippingFee("GFI", 1, 20, 10, 10)
-		self.assertEqual(shipping.get_fee(), "74.0")
+		self.assertEqual(shipping.get_fee(), ("74.0", "7.5%", "80.0"))
 
 	def test_MediumParcel_skdlks(self):
 		shipping = ShippingFee("skdlks", 1, 20, 10, 10)
-		self.assertEqual(shipping.get_fee(), "80.0")
+		self.assertEqual(shipping.get_fee(), ("80.0", "0%", "80.0"))
 
 
 	def test_LargeParcel_MYNT(self):
 		shipping = ShippingFee("MYNT", 1, 30, 10, 10)
-		self.assertEqual(shipping.get_fee(), "131.625")
+		self.assertEqual(shipping.get_fee(), ("131.625", "12.25%", "150.0"))
 
 	def test_LargeParcel_GFI(self):
 		shipping = ShippingFee("GFI", 1, 30, 10, 10)
-		self.assertEqual(shipping.get_fee(), "138.75")
+		self.assertEqual(shipping.get_fee(), ("138.75", "7.5%", "150.0"))
 
 	def test_LargeParcel_skdlks(self):
 		shipping = ShippingFee("skdlks", 1, 30, 10, 10)
-		self.assertEqual(shipping.get_fee(), "150.0")
+		self.assertEqual(shipping.get_fee(), ("150.0", "0%", "150.0"))
 
 
 if __name__ == "__main__":
